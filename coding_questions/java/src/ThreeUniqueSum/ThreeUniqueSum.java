d70efa5401126c3b02ee8fcd5fc499f45eaac8bf
gh0stl0nelyGit <khoinguyen19971997@gmail.com>
2021-05-05 23:48:01 -0400

    Fixed java solution for threeSum question

--- coding_questions/java/src/ThreeUniqueSum/ThreeUniqueSum.java
+++ coding_questions/java/src/ThreeUniqueSum/ThreeUniqueSum.java
@@ -3,18 +3,20 @@ package ThreeUniqueSum;
 import java.util.*;
 
 class ThreeUniqueSum {
-  private static Map<Integer, Integer> map = new HashMap<Integer,Integer>();
-  private static Set<List<Integer>> result = new HashSet<List<Integer>>();
 
   public static void main(String[] args) {
-    System.out.println(threeSum(new int[]{9,8,7,6,5,4,3,1,-4,-2})); 
-    System.out.println(threeSum(new int[]{-2,0,3,4,6,1})); 
-    System.out.println(threeSum(new int[]{0,0,0,0,0,0,0})); 
-    System.out.println(threeSum(new int[]{1,2})); 
+    System.out.println(threeSum(new int[]{9,8,7,6,5,4,3,1,-4,-2}));
+    System.out.println(threeSum(new int[] { -2, 0, 3, 4, 6, 1 }));
+    System.out.println(threeSum(new int[] { 0, 0, 0}));
+    System.out.println(threeSum(new int[]{1,2}));
   }
 
   public static List<List<Integer>> threeSum(int[] A) {
-    if(A.length < 2) return new ArrayList<List<Integer>>();
+     HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+     Set<List<Integer>> result = new HashSet<List<Integer>>();
+  
+    if (A.length < 2)
+      return new ArrayList<List<Integer>>();
 
     for (int i = 0; i < A.length; i++) {
       if (!map.containsKey(A[i])) {
@@ -22,12 +24,12 @@ class ThreeUniqueSum {
       }
     }
 
-    for (int i = 0; i < A.length - 2; i++) {
+    for (int i = 0; i < A.length - 1; i++) {
       int currI = A[i];
       for (int j = i + 1; j < A.length; j++) {
         int currJ = A[j];
         int complement = 0 - (currI) - (currJ);
-        if (isCombinationPossible(complement, i, j)) {
+        if (isCombinationPossible(complement, i, j, map)) {
           List<Integer> list = groupByDescending(currI, currJ, complement);
           result.add(list);
         }
@@ -35,7 +37,7 @@ class ThreeUniqueSum {
     }
 
     List<List<Integer>> real = new ArrayList<List<Integer>>();
-    
+
     for (List<Integer> list : result) {
       real.add(list);
     }
@@ -43,7 +45,7 @@ class ThreeUniqueSum {
     return real;
   }
 
-  private static boolean isCombinationPossible(int complement, int i1, int i2) {
+  private static boolean isCombinationPossible(int complement, int i1, int i2, HashMap<Integer,Integer> map) {
     return map.containsKey(complement) && map.get(complement) != i1 && map.get(complement) != i2;
   }
 
