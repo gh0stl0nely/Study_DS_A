e8949ce9d8233b52000cb0d379a4c80717543ad9
gh0stl0nelyGit <khoinguyen19971997@gmail.com>
2021-05-08 00:44:24 -0400

    implemented javascript solution for LinkedList

---
+++ coding_questions/javascript/src/LinkedList/solution.js
@@ -0,0 +1,147 @@
+const { Node } = require("./Node");
+
+class LinkedList {
+    constructor(){
+        this.size = 0;
+        this.head = null;
+        this.tail = null;
+        this.getHead.bind(this);
+        this.getTail.bind(this);
+        this.addToHead.bind(this);
+        this.addToTail.bind(this);
+        this.print.bind(this);
+        this.listSize.bind(this);
+        this.reverse.bind(this);
+        this.addToIndex.bind(this);
+        this.deleteAtIndex.bind(this);
+    }
+
+    getHead(){
+        return this.head;
+    }
+
+    getTail(){
+        return this.tail;
+    }
+
+    reverse(){
+        if(this.listSize() <= 1) return;
+
+        let curr = this.head;
+        let prev = null;
+        let next = null;
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        this.tail = this.head;
+        this.head = prev;
+    }
+
+    addToIndex(index, node){
+        if (node == null)
+            return;
+
+        if (index <= 0) {
+            this.addToHead(node);
+        } else if (index >= this.listSize()) {
+            this.addToTail(node);
+        } else {
+            let curr = this.head;
+            let counter = 0;
+            while (curr != null) {
+                if (counter + 1 == index) {
+                    node.next = curr.next;
+                    curr.next = node;
+                    size++;
+                    break;
+                }
+
+                curr = curr.next;
+                counter++;
+            }
+        }
+    }
+
+    deleteAtIndex(index){
+        if (index < 0 || index >= this.size()) {
+            return null;
+        }
+
+        let curr = this.head;
+
+        if (index == 0) {
+            let temp = curr;
+            this.head = curr.next;
+            curr = null;
+            size--;
+            return temp;
+        }
+
+        let counter = 0;
+        while (curr != null) {
+            if (counter + 1 == index) {
+                let temp = curr.next;
+                curr.next = curr.next.next;
+                if(counter + 1 == this.size() - 1){
+                    this.tail = curr;
+                }
+                size--;
+                return temp;
+            }
+
+            curr = curr.next;
+
+            counter++;
+        }
+
+        return null;
+    }
+
+    isEmpty(){
+        return this.listSize() == 0; 
+    }
+
+    listSize(){
+        return this.size;
+    }
+
+    print(){
+        let curr = this.head;
+
+        while(curr != null){
+            console.log(curr.val);
+            curr = curr.next;
+        }
+    }
+
+    addToHead(node){
+        if (this.isEmpty()) {
+            this.head = node;
+            this.tail = node;
+        } else {
+            let curr = this.head;
+            node.next = curr;
+            this.head = node;
+        }
+        this.size++;
+    }
+
+    addToTail(node){
+        if (this.isEmpty()) {
+            this.head = node;
+            this.tail = node;
+        } else {
+            let curr = this.tail;
+            curr.next = node;
+            node.next = null;
+            this.tail = node;
+        }
+        this.size++;
+    }
+}
+
+module.exports = LinkedList;
---
+++ coding_questions/javascript/src/LinkedList/test.js
@@ -0,0 +1,32 @@
+const LinkedList = require("./solution");
+const Node = require("./Node");
+
+
+
+test("return the correct head", () => {
+    let mainList = new LinkedList();
+    mainList.addToTail(new Node(2));
+    mainList.addToTail(new Node(3));
+    expect()
+
+});
+
+
+test("return the null head", () => {
+    let mainList = new LinkedList();
+});
+
+
+test("return the correct tail", () => {
+    let mainList = new LinkedList();
+});
+
+
+test("return the null tail", () => {
+    let mainList = new LinkedList();
+});
+
+
+test("", () => {
+    let mainList = new LinkedList();
+});
